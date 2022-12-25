338af9794f93570f5758a7bc3f3288f321c4ce7c
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2022-12-24 19:00:05 -0500

    Move points' inits on one line

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -26,62 +26,30 @@ fn search(init_start: Point, init_end: Point, map: &Vec<Vec<char>>, blizzards: &
                 match b.val {
                     '>' => {
                         if b.x + 1 != map[0].len() as i32 - 1 {
-                            new_blizs.insert(Point {
-                                x: b.x + 1,
-                                y: b.y,
-                                val: '>',
-                            });
+                            new_blizs.insert(Point { x: b.x + 1, y: b.y, val: '>' });
                         } else {
-                            new_blizs.insert(Point {
-                                x: 1,
-                                y: b.y,
-                                val: '>',
-                            });
+                            new_blizs.insert(Point { x: 1, y: b.y, val: '>' });
                         }
                     }
                     '<' => {
                         if b.x != 1 {
-                            new_blizs.insert(Point {
-                                x: b.x - 1,
-                                y: b.y,
-                                val: '<',
-                            });
+                            new_blizs.insert(Point { x: b.x - 1, y: b.y, val: '<' });
                         } else {
-                            new_blizs.insert(Point {
-                                x: map[0].len() as i32 - 2,
-                                y: b.y,
-                                val: '<',
-                            });
+                            new_blizs.insert(Point { x: map[0].len() as i32 - 2, y: b.y, val: '<' });
                         }
                     }
                     'v' => {
                         if b.y + 1 != map.len() as i32 - 1 {
-                            new_blizs.insert(Point {
-                                x: b.x,
-                                y: b.y + 1,
-                                val: 'v',
-                            });
+                            new_blizs.insert(Point { x: b.x, y: b.y + 1, val: 'v' });
                         } else {
-                            new_blizs.insert(Point {
-                                x: b.x,
-                                y: 1,
-                                val: 'v',
-                            });
+                            new_blizs.insert(Point { x: b.x, y: 1, val: 'v' });
                         }
                     }
                     '^' => {
                         if b.y != 1 {
-                            new_blizs.insert(Point {
-                                x: b.x,
-                                y: b.y - 1,
-                                val: '^',
-                            });
+                            new_blizs.insert(Point { x: b.x, y: b.y - 1, val: '^' });
                         } else {
-                            new_blizs.insert(Point {
-                                x: b.x,
-                                y: map.len() as i32 - 2,
-                                val: '^',
-                            });
+                            new_blizs.insert(Point { x: b.x, y: map.len() as i32 - 2, val: '^' });
                         }
                     }
                     _ => panic!("Invalid blizzard!"),
@@ -96,11 +64,7 @@ fn search(init_start: Point, init_end: Point, map: &Vec<Vec<char>>, blizzards: &
                     .iter()
                     .for_each(|(x, y)| {
                         if ['>', '<', 'v', '^'].iter().all(|d| {
-                            !blizs.contains(&Point {
-                                x: path.x + *x,
-                                y: path.y + *y,
-                                val: *d,
-                            })
+                            !blizs.contains(&Point { x: path.x + *x, y: path.y + *y, val: *d })
                         }) {
                             if (path.x + *x >= 1
                                 && path.x + *x < map[0].len() as i32 - 1
@@ -113,11 +77,7 @@ fn search(init_start: Point, init_end: Point, map: &Vec<Vec<char>>, blizzards: &
                                 if path.x + *x == end.x && path.y + *y == end.y {
                                     quit = true;
                                 }
-                                new_paths.insert(Point {
-                                    x: path.x + *x,
-                                    y: path.y + *y,
-                                    val: path.val,
-                                });
+                                new_paths.insert(Point { x: path.x + *x, y: path.y + *y, val: path.val });
                             }
                         }
                     });
@@ -139,20 +99,12 @@ fn main() {
         .collect::<Vec<Vec<char>>>();
 
     let mut blizs = HashSet::new();
-    let me = Point {
-        x: 1,
-        y: 0,
-        val: 'E',
-    };
+    let me = Point { x: 1, y: 0, val: 'E' };
 
     for (row, v) in map.iter().enumerate() {
         for (col, c) in v.iter().enumerate() {
             if c.ne(&'.') && c.ne(&'#') {
-                blizs.insert(Point {
-                    x: col as i32,
-                    y: row as i32,
-                    val: *c,
-                });
+                blizs.insert(Point { x: col as i32, y: row as i32, val: *c });
             }
         }
     }
